9ff680bb3bf1e2cb0d0609d22fb9f18a7f574fbd
Thiagomrfs <thiagomarbsn@gmail.com>
2023-01-08 16:08:07 -0300

    Todos app

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    priorities = (
+        ("A", "URGENTE"),
+        ("B", "MODERADO"),
+        ("C", "LEVE"),
+    )
+
+    nome = models.CharField(max_length=20)
+    description = models.CharField(max_length=50)
+    priority = models.CharField(max_length=1, choices=priorities)
+    completo = models.BooleanField(default=False)
--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Todo
+from .serializer import TodoSerializer
+
+class ViewTodos(generics.ListCreateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+class ViewTodo(generics.RetrieveDestroyAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+    def get(self, request, pk):
+        try:
+            instance = Todo.objects.get(pk=pk)
+            serializer = TodoSerializer(instance)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except:
+            return Response({"Erro": "Não encontrado!"}, status=status.HTTP_404_NOT_FOUND)
+    
+    def patch(self, request, pk):
+        try:
+            instance = Todo.objects.get(pk=pk)
+            serializer = TodoSerializer(instance, data=request.data, partial=True)
+
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_206_PARTIAL_CONTENT)
+            
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except:
+            return Response({"Erro": "Não encontrado!"}, status=status.HTTP_404_NOT_FOUND)
+
